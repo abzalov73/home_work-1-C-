cd4fba648fe7eb8e049fe9981e8956d63d7490c9
Ruslan Abzalov <abzalov73ruslan@gmail.com>
2023-02-05 18:27:05 +0400

    home_work 1

---
+++ task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает какое число больше, а какое меньше
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine($"{a} > {b}");
+}
+if(a < b)
+{
+   Console.WriteLine($"{b} > {a}"); 
+}
+else
+{
+    Console.WriteLine($"{a} = {b}");
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает три числа и выдает максимальное из этих чисел
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if(a > b & a > c)
+{
+    Console.WriteLine(a);
+}
+if(b > a & b > c)
+{
+     Console.WriteLine(b);
+}
+if(c > a & c > b)
+{
+     Console.WriteLine(c);
+}
